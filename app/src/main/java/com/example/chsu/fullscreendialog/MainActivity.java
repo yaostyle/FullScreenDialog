02ebe216ba0906fe40c8b715c888a5eb5fb8dfc8
chsu <yaostyle@gmail.com>
2018-08-16 15:51:54 -0700

    Showing custom dialog by trigger showDialog() custom function.

--- app/src/main/java/com/example/chsu/fullscreendialog/MainActivity.java
+++ app/src/main/java/com/example/chsu/fullscreendialog/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chsu.fullscreendialog;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -10,6 +11,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -19,9 +21,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button openDialog;
-    Dialog dialog;
-    CustomDialogFragment dialogFragment;
+    static Button openDialog;
+    Button closeDialog;
+    static Dialog dialog;
+    static CustomDialogFragment dialogFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,36 +34,40 @@ public class MainActivity extends AppCompatActivity {
 
         openDialog = findViewById(R.id.open);
 
-        View view = getLayoutInflater().inflate(R.layout.full_screen, null);
-        dialog = new Dialog(this, android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar);
-
-        rLayout = view.findViewById(R.id.rLayout);
-
         openDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openDialog.setVisibility(View.INVISIBLE);
+                //openDialog.setVisibility(View.INVISIBLE);
                 showDialog();
-                Toast.makeText(MainActivity.this, "Tap to close", Toast.LENGTH_SHORT).show();
             }
         });
 
-        rLayout.setOnClickListener(new View.OnClickListener() {
+        View dialogView = getLayoutInflater().inflate(R.layout.full_screen, null);
+
+        Button closeDialog = dialogView.findViewById(R.id.close);
+
+        closeDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //dialog.dismiss();
-                dialogFragment.dismiss();
-
+                Toast.makeText(MainActivity.this,
+                        "Close button click...",
+                        Toast.LENGTH_SHORT).show();
+                dialogFragment.dismissAllowingStateLoss();
             }
         });
     }
 
     public static class CustomDialogFragment extends DialogFragment {
+        public CustomDialogFragment() {
+            //Empty
+        }
+
         @Nullable
         @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-            return inflater.inflate(R.layout.full_screen, container, false);
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.full_screen, container, false  );
         }
 
         @NonNull
@@ -68,8 +75,10 @@ public class MainActivity extends AppCompatActivity {
         public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
             Dialog dialog = super.onCreateDialog(savedInstanceState);
             dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
             return dialog;
         }
+
     }
 
     public void showDialog() {
@@ -79,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_right);
-
         transaction.add(android.R.id.content, dialogFragment).addToBackStack(null).commit();
     }
 }

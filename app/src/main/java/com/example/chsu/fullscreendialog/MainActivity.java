ae49048d02e2abf448da8f1c7e39620d73ba392b
chsu <yaostyle@gmail.com>
2018-08-16 11:55:26 -0700

    Created CustomDialogFragment, set onClicklistener to a button to trigger a dialog window.

--- app/src/main/java/com/example/chsu/fullscreendialog/MainActivity.java
+++ app/src/main/java/com/example/chsu/fullscreendialog/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.chsu.fullscreendialog;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button openDialog;
+    Dialog dialog;
+    CustomDialogFragment dialogFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RelativeLayout rLayout;
+
+        openDialog = findViewById(R.id.open);
+
+        View view = getLayoutInflater().inflate(R.layout.full_screen, null);
+        dialog = new Dialog(this, android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar);
+
+        rLayout = view.findViewById(R.id.rLayout);
+
+        openDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+                Toast.makeText(MainActivity.this, "Tap to close", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //dialog.dismiss();
+                dialogFragment.dismiss();
+
+            }
+        });
+    }
+
+    public static class CustomDialogFragment extends DialogFragment {
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.full_screen, container, false);
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            Dialog dialog = super.onCreateDialog(savedInstanceState);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            return dialog;
+        }
+    }
+
+    public void showDialog() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        dialogFragment = new CustomDialogFragment();
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.add(android.R.id.content, dialogFragment).addToBackStack(null).commit();
     }
 }
